362a8edf92f306adf250fa7675d87648dbef1192
gunnargray-dev <hi@gunnargray.com>
2025-01-15 11:16:18 -0600

    Initial Commit

---
+++ Dot Grid Shaders/Dot_Grid_ShadersApp.swift
@@ -0,0 +1,32 @@
+//
+//  Dot_Grid_ShadersApp.swift
+//  Dot Grid Shaders
+//
+//  Created by Gunnar Gray on 1/15/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Dot_Grid_ShadersApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
